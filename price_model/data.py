e862544c35dd55cec13408dc12f082a4c4c7c490
kdrivas <k.rivas@pucp.edu.pe>
2025-02-11 18:45:23 -0500

    Cast bool cols

--- price_model/data.py
+++ price_model/data.py
@@ -29,8 +29,11 @@ def create_features(df: pd.DataFrame) -> pd.DataFrame:
 
     df["retailer_l"] = df["retailer"].str.lower()
     for col in ONE_HOT_RETAILER_COLS:
-        df[col] = df["retailer_l"].str.contains(col)
-        df[col] = df[col].astype(bool)
+        df[col] = df["retailer_l"].str.contains(
+            col,
+            case=False,
+            na=False,
+        )
 
     reg = r"|".join(ONE_HOT_RETAILER_COLS)
     df["other_retailer"] = ~df["retailer_l"].str.contains(
@@ -41,8 +44,11 @@ def create_features(df: pd.DataFrame) -> pd.DataFrame:
 
     df["color_l"] = df["color"].str.lower()
     for col in ONE_HOT_COLOR_COLS:
-        df[col] = df["color_l"].str.contains(col)
-        df[col] = df[col].astype(bool)
+        df[col] = df["color_l"].str.contains(
+            col,
+            case=False,
+            na=False,
+        )
 
     reg = r"|".join(ONE_HOT_COLOR_COLS)
     df["other_color"] = ~df["color_l"].str.contains(
